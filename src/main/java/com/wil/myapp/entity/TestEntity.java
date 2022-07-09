1ecb437dc2a2fe16e601fb2d1c88f3bb263e4591
Wilson Brito <wil.brito@icloud.com>
2022-07-09 18:43:42 +0200

    Primer commit: my-app

---
+++ src/main/java/com/wil/myapp/entity/TestEntity.java
@@ -0,0 +1,25 @@
+package com.wil.myapp.entity;
+
+public class TestEntity {
+
+	private String nombre;
+	
+	
+	
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
